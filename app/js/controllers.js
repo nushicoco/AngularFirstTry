69ad6b628201daac778ca4273334e0b86363e49b
Nushicarmon <Nushicarmon@gmail.com>
2013-05-01 00:25:46 +0300

    Initial commit

---
+++ app/js/controllers.js
@@ -0,0 +1,11 @@
+alert("here");
+function SmallKnowiiCntl($scope) {
+	$scope.knowiis = [{
+	"Raiting" : 3,
+	"Date" : "23/01/2012",
+	"PublisherName" : "Uri Dudai",
+	"Image-src" : "img/preview.jpg",
+	"Preview-txt" : "Craftsy Workshops are tutorials from amazing designers that include the pattern, instructions, and step-by-step photos. And, the BEST part: you'll never get stuck...just ask the pattern designer for help!"
+}];
+  $scope.cost = 19.95;
+} 
